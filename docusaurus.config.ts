900c28f96b6dbeee53c80ce5d16eb575c01eabf9
Rishi Panthee <github@rishipanthee.com>
2025-04-15 00:27:43 -0400

    Add Analytics

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
@@ -28,6 +28,8 @@ const config: Config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
+
+  scripts: [{ src: 'https://a.dbuidl.com/js/script.js', defer: true, 'data-domain': 'ncesk8.com' }],
 
   presets: [
     [
@@ -98,49 +100,49 @@ const config: Config = {
       ],
     },
     // footer: {
-    //   style: 'dark',
-    //   links: [
-    //     {
-    //       title: 'Docs',
-    //       items: [
-    //         {
-    //           label: 'Tutorial',
-    //           to: '/docs/intro',
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: 'Community',
-    //       items: [
-    //         {
-    //           label: 'Stack Overflow',
-    //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-    //         },
-    //         {
-    //           label: 'Discord',
-    //           href: 'https://discordapp.com/invite/docusaurus',
-    //         },
-    //         {
-    //           label: 'Twitter',
-    //           href: 'https://twitter.com/docusaurus',
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: 'More',
-    //       items: [
-    //         {
-    //           label: 'Blog',
-    //           to: '/blog',
-    //         },
-    //         {
-    //           label: 'GitHub',
-    //           href: 'https://github.com/facebook/docusaurus',
-    //         },
-    //       ],
-    //     },
-    //   ],
-    //   // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    // style: 'dark',
+    // links: [
+    //   {
+    //     title: 'Docs',
+    //     items: [
+    //       {
+    //         label: 'Tutorial',
+    //         to: '/docs/intro',
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     title: 'Community',
+    //     items: [
+    //       {
+    //         label: 'Stack Overflow',
+    //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+    //       },
+    //       {
+    //         label: 'Discord',
+    //         href: 'https://discordapp.com/invite/docusaurus',
+    //       },
+    //       {
+    //         label: 'Twitter',
+    //         href: 'https://twitter.com/docusaurus',
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     title: 'More',
+    //     items: [
+    //       {
+    //         label: 'Blog',
+    //         to: '/blog',
+    //       },
+    //       {
+    //         label: 'GitHub',
+    //         href: 'https://github.com/facebook/docusaurus',
+    //       },
+    //     ],
+    //   },
+    // ],
+    // copyright: `Copyright © ${new Date().getFullYear()} NCESK8`,
     // },
     prism: {
       theme: prismThemes.github,
